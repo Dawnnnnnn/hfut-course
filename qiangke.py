3e21dd7c5c461105d13fde74015f48dfb27bbccd
Dawnspace <1050596704@qq.com>
2017-12-30 11:11:21 +0800

    rmb122 NB!

--- qiangke.py
+++ qiangke.py
@@ -1,38 +1,65 @@
 import requests
 import time
-# cookies值失效时间暂时不确定，应该在一天之内
-cookies = ''
-# 这是每个人的固定 studentId
-studentAssoc = ''
-# 这是一个固定值，暂不知道干嘛用的
-courseSelectTurnAssoc = '161'
-# 这也是一个固定值，暂不知道干嘛用的
+import hashlib
+import json
+
+
+# 这是课程代码,你只需要修改这里
+lessonAssoc = [246862, 246865]
+
+
+username = input("快输入你的学号:")
+password = input("再输入你的密码我就能盗号了:")
+
+
+# 这是一个非固定值，看起来像是选课轮次，或许明年一轮直接用163就能必中课程了吧
+courseSelectTurnAssoc = '163'
+# 这也是一个固定值，看起来像照搬的别的系统，没有删干净
 virtualCost = '0'
 
-# 这是课程代码
-lessonAssoc = []
 
-headers = {
-    'Cookie': cookies,
-    "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-}
-
-
-def Add_course(headers):
+def Add_course(username,password):
+    salt_url = 'http://jxglstu.hfut.edu.cn/eams5-student/login-salt'
+    s = requests.Session()
+    response = s.get(salt_url)
+    while response.status_code == 404:
+        s = requests.Session()
+        response = s.get(salt_url)
+    temp = response.text
+    password = temp + "-" +password
+    hash = hashlib.sha1()
+    hash.update(password.encode('utf-8'))
+    password = hash.hexdigest()
+    data = {'username': username, 'password': password, 'captcha': ''}
+    data = json.dumps(data, separators=(',', ':')).encode(encoding='utf-8')
+    header = {'Content-Type': 'application/json'}
+    login_url = "http://jxglstu.hfut.edu.cn/eams5-student/login"
+    s.post(login_url,data=data,headers=header)
     url = 'http://jxglstu.hfut.edu.cn/eams5-student/ws/for-std/course-select/add-request'
     url1 = 'http://jxglstu.hfut.edu.cn/eams5-student/ws/for-std/course-select/add-drop-response'
+    url2 = 'http://jxglstu.hfut.edu.cn/eams5-student/ws/for-std/course-select/std-count'
+    s.get('http://jxglstu.hfut.edu.cn/eams5-student/for-std/course-select')
+    info_url = 'http://jxglstu.hfut.edu.cn/eams5-student/for-std/student-info'
+    res = s.get(info_url)
+    id = res.url[-5:]
+
     for i in lessonAssoc:
-        data = {'studentAssoc': studentAssoc, 'lessonAssoc': i,
+        data = {'studentAssoc': id, 'lessonAssoc': i,
                 'courseSelectTurnAssoc': courseSelectTurnAssoc, 'scheduleGroupAssoc': '', 'virtualCost': virtualCost}
-        response = requests.post(url, data=data, headers=headers)
+        response = s.post(url, data=data)
         temp = response.text
-        data1 = {'studentId': studentAssoc, 'requestId': temp}
-        response1 = requests.post(url1, data=data1, headers=headers)
-        try:
-            print(response1.json())
-        except:
-            print(response1.text)
+        data1 = {'studentId': id, 'requestId': temp}
+        response1 = s.post(url1, data=data1)
+        data2 = {'lessonIds[]':i}
+        response2 = s.post(url2,data=data2)
+        print("选课是否成功:",response1.json()['success'])
+        temp = response2.json()
+        value = list(temp.values())
+        print("当前课程选课人数:",value)
         time.sleep(0.01)
 
 
-Add_course(headers)
+while 1:
+    Add_course(username,password)
+    time.sleep(0.01)
+
